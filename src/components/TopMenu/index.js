7a0fa25b6afeebe9136761b20f5b99e305b5ea2c
yugao <gao.yu1@northeastern.edu>
2022-01-21 14:07:23 -0800

    original scss

---
+++ src/components/TopMenu/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './_topmenu.scss';
+
+const menu = ['Men', 'Women', 'Children', 'bestSell'];
+
+function index() {
+    return (
+        <>
+            <div className="container-fluid">
+                <div className="row">
+                    <div className="col-xl-12 col-lg-12">
+                        <nav>
+                            <ul>
+
+                            </ul>
+                        </nav>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+}
+
+export default index;
